23995af28bc6ab024b9c964f05625726018b955e
AliKHalili <30647838+AliKHaliliT@users.noreply.github.com>
2023-05-04 10:18:36 +0330

    Updated versions

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ def readme():
 
 # Defining the setup function
 setup(name='CVAugmentor',
-        version='1.0.11',
+        version='1.0.12',
         description='A package for augmenting images and videos for computer vision tasks',
         long_description=readme(),
         long_description_content_type='text/markdown',
