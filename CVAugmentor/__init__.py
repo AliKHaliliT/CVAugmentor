8864414a9747a1804315f141054174ff4520d2be
AliKHalili <30647838+AliKHaliliT@users.noreply.github.com>
2025-02-07 12:01:38 +0330

    Updated tests workflow

--- CVAugmentor/__init__.py
+++ CVAugmentor/__init__.py
@@ -4,7 +4,7 @@ The CVAugmentor Augmentation Package.
 
 
 CVAugmentor is a Python package designed for augmenting images and videos, making it easier to enhance and modify visual data for computer vision tasks. 
-It provides a collection of utilities that automate transformations such as flipping, rotation, scaling, color adjustments, and more.
+It provides a collection of utilities that automate transformations such as flipping, rotation, scaling, color adjustments, and more. 
 
 """
 from .assets.main.pipeline import Pipeline
